f16c493f3f3000747f50754786bd3dd341e5dade
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-04-19 20:00:21 -0500

    5 - Plantilla html

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -2,7 +2,9 @@
     class Landing{
         public function __construct(){}
         public function main(){
+            require_once "views/roles/business/header.view.php";
             require_once "views/business/index.view.php";
+            require_once "views/roles/business/footer.view.php";
         }
     }
 ?>

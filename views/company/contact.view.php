f6429ead62b1016f8b225dbd330afde12520f4a2
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-05-15 09:33:33 -0500

    04 - Controladores y acciones (Clases y Métodos)

--- views/company/contact.view.php
+++ views/company/contact.view.php
@@ -1,5 +1,33 @@
 <section class="borde">
-    <form action="" method="">
-        <h1>Hola</h1>
+    <form action="" method="" class="borde">
+        <div class="borde form-header">
+            <h2>Contáctanos</h2>
+        </div>
+        <div class="borde form-body">
+            <div class="form-control">
+                <label for="">Nombres</label>
+                <input type="text" placeholder="Nombres">
+            </div>
+            <div class="form-control">
+                <label for="">Apellidos</label>
+                <input type="text" placeholder="Apellidos">
+            </div>
+            <div class="form-control">
+                <label for="">Email</label>
+                <input type="text" placeholder="Email">
+            </div>
+            <div class="form-control">
+                <label for="">Asunto</label>
+                <input type="text" placeholder="Email">
+            </div>
+            <div class="form-control">
+                <label for="" class="label-textarea">Mensaje</label>                
+                <textarea></textarea>
+            </div>
+        </div>
+        <div class="borde form-footer">
+            <input type="reset" value="Cancelar" class="borde">
+            <input type="submit" value="Enviar" class="borde">
+        </div>
     </form>
 </section>

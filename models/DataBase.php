3a535eefc57aa11cefdc1be3878597d7dc3ee465
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-05-25 04:44:40 -0500

    despliegue

--- models/DataBase.php
+++ models/DataBase.php
@@ -8,7 +8,7 @@
             $port = "3306";
             $database = "dbappwebinv";
             $username = "admin_albeiro";
-            $password = "Earv1234";
+            $password = "SoyGenio12345";
 			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;

006f24119f59ccdefd47cf228a58730b47f1c72f
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-05-24 21:23:12 -0500

    despliegue

--- models/DataBase.php
+++ models/DataBase.php
@@ -2,7 +2,7 @@
     class DataBase{
         public static function connection(){
             $hostname = "dbappwebinv.mysql.database.azure.com";
-            $database = "dbappwebinvs";
+            $database = "dbappwebinv";
             $username = "admin_albeiro";
             $password = "Earv1234";
 			$pdo = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8",$username,$password);
